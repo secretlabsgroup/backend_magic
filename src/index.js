6ea766ece9eac4301f2adf984055a075b25b3af1
Holden B <holden.bucher.92@gmail.com>
2019-02-12 14:24:17 -0600

    signup, signin, and createEvent working

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
 
-require('dotenv').config();
+require('dotenv').config({ path: '.env' });
 const createServer = require('./createServer');
-const db = require('./db');
 
 const server = createServer();
-
 server.express.use(cookieParser());
 
-server.start(
-	// {
-	//   cors: {
-	//     credentials: true,
-	//     origin: process.env.FRONTEND_URL
-	//   }
-	// },
-	deets => {
-		console.log(`Server is now running on port http://localhost:${deets.port}`);
-	}
-);
+server.start(deets => {
+	console.log(`Server is now running on port http://localhost:${deets.port}`);
+});
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,6 +1,54 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const Mutation = {};
+const Mutation = {
+	async createEvent(parent, args, ctx, info) {
+		const event = await ctx.db.mutation.createEvent(
+			{
+				data: { ...args }
+			},
+			info
+		);
+		console.log(event);
+		return event;
+	},
+	async signup(parent, args, ctx, info) {
+		args.email = args.email.toLowerCase();
+		const password = await bcrypt.hash(args.password, 10);
+		const user = await ctx.db.mutation.createUser(
+			{
+				data: {
+					...args,
+					password,
+					permissions: { set: ['FREE'] }
+				}
+			},
+			info
+		);
+		const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+		ctx.response.cookie('token', token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+		});
+		// return user
+		return user;
+	},
+	async signin(parent, { email, password }, ctx, info) {
+		const user = await ctx.db.query.user({ where: { email } });
+		if (!user) {
+			throw new Error(`No such user found for email ${email}`);
+		}
+		const valid = await bcrypt.compare(password, user.password);
+		if (!valid) {
+			throw new Error('Invalid Password!');
+		}
+		const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+		ctx.response.cookie('token', token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365
+		});
+		return user;
+	}
+};
 
 module.exports = Mutation;
