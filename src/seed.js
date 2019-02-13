07c75ed83475c913edc4d761dee0d52467082c22
Holden B <holden.bucher.92@gmail.com>
2019-02-12 22:23:56 -0600

    seed users byahhh

--- src/seed.js
+++ src/seed.js
@@ -1,16 +1,25 @@
 require('dotenv').config({ path: '.env' });
 const db = require('./db');
-const seedUsers = require('./seedUsers');
+const bcrypt = require('bcryptjs');
+let seedUsers = require('./seedUsers');
+// WARNING WARNING WARNING WARNING WARNING WARNING WARNING
+// gotta seed 100 users at a time because docker doesn't like you otherwise
+seedUsers = seedUsers.slice(0, 100);
 
 const seedDatabase = async () => {
 	Promise.all(
 		seedUsers.map(async user => {
 			delete user.id;
 			user.gender = user.gender.toUpperCase();
+			const password = await bcrypt.hash(user.password, 10);
 			try {
-				const response = await db.mutation.createUser({
+				const response = await db.mutation.updateUser({
 					data: {
-						...user
+						...user,
+						password
+					},
+					where: {
+						email: user.email
 					}
 				});
 				return response;
